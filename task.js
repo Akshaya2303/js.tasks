eb1fcdefb7680acac4eabd53e96f8fe24c1fe2f3
Akshaya2303 <akshayanayakidi@gmail.com>
2024-06-17 23:08:49 +0530

    Tasks

--- task.js
+++ task.js
@@ -12,9 +12,9 @@
 // console.log(100/"0");
 // console.log(100 + +"100" - "100" * "100");
 // console.log(1 == "1");
-console.log(1 === "1");
-console.log(1 == "one");
-console.log(1 === "one");
+// console.log(1 === "1");
+// console.log(1 == "one");
+// console.log(1 === "one");
 // console.log(1+true);
 // console.log(1 - true);
 // console.log(1 + true - false);
@@ -40,3 +40,58 @@ console.log(1 === "one");
 // console.log(typeof 1n+2n);
 // console.log(typeof 1+2n);
 // console.log(typeof 1/1n);
+
+
+// console.log(null == undefined);
+// console.log( [] == false); 
+// console.log({} + {}) ;
+// console.log(typeof 42n) ;
+// console.log("" == false) ;
+// console.log(+'') ;
+// console.log(Boolean("0")) ;
+// console.log([] === []) ;
+// console.log(typeof Infinity) ;
+
+// Write a program to find the sum of all even numbers in an array using a for loop.
+
+//   Example: let arr = [1, 2, 3, 4, 5, 6];
+// 
+
+// 2.Use a do-while loop to print the numbers from 1 to 10, but only if 
+// the number is not equal to 5.
+// Example: let num = 1;
+// Test Case: printNumbers()
+
+// function primenum(){
+// let num=1;
+// do{
+//     if(num!=5){
+//         console.log(num);
+//     }
+//     num++;
+// }
+// while(num<=10);
+// }
+// primenum();
+
+// 6.
+// *
+// **
+// *
+// **
+// ***
+
+function printPattern() {
+    let pattern = [1, 2, 1, 2, 3];
+  
+    for (let i = 0; i < pattern.length; i++) {
+      let stars = '';
+      for (let j = 0; j < pattern[i]; j++) {
+        stars += '*';
+      }
+      console.log(stars);
+    }
+  }
+  
+  printPattern();
+  
